5f1218417a0dc7db82b3281b383fa96a66d62913
bwa <40144023+beaesq@users.noreply.github.com>
2023-11-12 10:30:16 +0800

    Add viewport meta tag & icons to header

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@ import template from "./template.html";
 import "./style.css";
 
 window.onload = () => {
+  const meta = document.createElement('meta');
+  meta.setAttribute('name', 'viewport');
+  meta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+  document.head.appendChild(meta);
+
+  const devicon = document.createElement("link");
+  devicon.setAttribute("rel", "stylesheet");
+  devicon.setAttribute("href", "https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css");
+  document.head.appendChild(devicon);
+
+  const iconify = document.createElement("script");
+  iconify.setAttribute("src", "//code.iconify.design/1/1.0.6/iconify.min.js");
+  document.head.appendChild(iconify);
+
   document.body.innerHTML = template;
 }
